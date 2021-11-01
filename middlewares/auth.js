179d7a353b17f23bf8668d0395f71fce2a7628a3
lam ha <lam.ha@team.eyeq.tech>
2021-11-01 21:56:32 +0000

    add token authentication middleware for room CRUD

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+const auth = async (req, res, next) => {
+  try {
+    // check token
+    // console.log("The current token \n", req.headers);
+    // split token from authorization
+    const token = req.headers.authorization.split(' ')[1];
+
+    let decodedData;
+    //verify and assign decoded id to userId
+    // if token exists
+    if (token) {
+      // verify token with secret text and assign data to decodedData
+      decodedData = jwt.verify(token, process.env.KEY_TOKEN);
+      // add decoded's id to userId of req object
+      req.userID = decodedData?.id;
+      req.userName = decodedData?.name;
+    }
+    next();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default auth;
