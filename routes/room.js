e092282450a9a980027ab57b762f2d19d73a930d
lam ha <lam.ha@team.eyeq.tech>
2021-11-01 21:57:31 +0000

    build routers for user and CRUD room

--- routes/room.js
+++ routes/room.js
@@ -1,5 +1,21 @@
 import express from 'express';
 
+// import action controllers
+import {
+  getAllRooms,
+  createRoom,
+  updateRoom,
+  deleteRoom,
+} from '../controllers/room.js';
+
+// import authentication
+import auth from '../middlewares/auth.js';
+
 const router = express.Router();
+
+router.get('/', auth, getAllRooms);
+router.post('/', auth, createRoom);
+router.patch('/:roomName', auth, updateRoom);
+router.delete('/:roomName', auth, deleteRoom);
 
 export default router;
