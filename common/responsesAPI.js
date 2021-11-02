ab91f4ba4a08ddd3c2bca2bed3f71f501f676bf8
lam ha <lam.ha@team.eyeq.tech>
2021-11-02 06:56:47 +0000

    modify response API statuscode

--- common/responsesAPI.js
+++ common/responsesAPI.js
@@ -1,15 +1,15 @@
-const responseGenerator = (statusCode = 400, data = {}) => {
+const responseGenerator = (statusCode = 400, res, data = {}) => {
   return res.status(statusCode).json(data);
 };
 
 const responses = {
-  _200: data => responseGenerator(200, data), // OK
-  _201: data => responseGenerator(201, data), // Created
-  _204: data => responseGenerator(204, data), // No Content
-  _400: data => responseGenerator(400, data), // Bad Request
-  _401: data => responseGenerator(401, data), // Unauthorized
-  _404: data => responseGenerator(404, data), // Not Found
-  _500: data => responseGenerator(500, data), // Internal Server Error
+  _200: (res, data) => responseGenerator(200, res, data), // OK
+  _201: (res, data) => responseGenerator(201, res, data), // Created
+  _204: (res, data) => responseGenerator(204, res, data), // No Content
+  _400: (res, data) => responseGenerator(400, res, data), // Bad Request
+  _401: (res, data) => responseGenerator(401, res, data), // Unauthorized
+  _404: (res, data) => responseGenerator(404, res, data), // Not Found
+  _500: (res, data) => responseGenerator(500, res, data), // Internal Server Error
 };
 
 export default responses;
